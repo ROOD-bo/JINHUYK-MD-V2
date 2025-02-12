d5ccf367272464ee41c099a79b5e5ad433c3d6af
ROOD-bo <lelejuvens@gmail.com>
2025-02-12 18:41:29 -0500

    Update config.js

--- config.js
+++ config.js
@@ -26,9 +26,9 @@ function convertToBool(text, fault = 'true') {
 //--------------------------Kang Jinhuyk---------------------------------------
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "YOUR JINHUYK SESSION PLACE IT HERE",
+SESSION_ID: process.env.SESSION_ID || "jIgW3JRC#t6eDqshjZYT2YWDuNeUGC3u4oGl0NWcjmBKZlQdku7M  ",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
-OWNER_NUMBER: process.env.OWNER_NUMBER || "242067274660",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50937529194",
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
